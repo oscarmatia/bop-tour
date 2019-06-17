fa3f6d5553f5be18fa466a8a68504b38f4fec25e
braggio2 <50121186+braggio2@users.noreply.github.com>
2019-06-16 21:02:38 -0400

    Update gestionar_tour.php

--- gestionar_tour.php
+++ gestionar_tour.php
@@ -1,17 +1,11 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
-
 	<meta charset="UTF-8">
-	<title>Gestionar</title>
-	<link rel="stylesheet" type="text/css" href="gestionar_tour.css">
+	<title>Document</title>
 </head>
+<link rel="stylesheet" type="text/css" href="gestionar_tour.css">
 <body>
-
 
 	<img src="camino_campo.jpg" alt="" class="imagen_fondo">
 	<div class="barra_navegacion">
@@ -35,11 +29,16 @@ session_start();
 			<input type="button" value="Cerrar Sesion" id="botonsito" onclick="redireccion_cerrar_sesion()"> 
 		</div>
 	</form>
+
 	<div class="contenedor_principal">
+
+
 		<h3 style="color: white; margin-top: 1%; margin-left: 3%;">Planifica tu viaje</h3>
-		<h5 style="color: wheat; margin-top: 1%; margin-left: 3%;">¿Dónde quieres ir?'</h5>
+		<h5 style="color: wheat; margin-top: 1%; margin-left: 3%;">¿Dónde quieres ir?</h5>
 		<hr style="color: wheat; margin-top: 2%;">
-		
+
+
+
 		<div class="seccion1">
 
 			<div class="cont-dest">     
@@ -50,9 +49,6 @@ session_start();
 			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12758.068648774599!2d-73.02870939999998!3d-36.92580725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2scl!4v1560717950496!5m2!1ses!2scl" width="400" height="250" frameborder="0" style="border:0;" class="mapa" allowfullscreen></iframe>
 
 		</div>
-
-
-
 
 		<div class="seccion2">
 
@@ -70,68 +66,27 @@ session_start();
 				<p class="cant2 ">Cantidad</p>        
 				<input name="cantidad2"id="cantidad2" type="text"  required>  
 			</div>	
-
-
-
+			<a  href="gestionar_tour.php">
+				<input type="button" value="Gestionar" style="color: white; background-color: green; border: solid; border-color: green; text-align: center; margin-top: 12%; margin-left: 50%; padding-left: 4%; padding-right: 5%;padding-top: 2%; padding-bottom: 2%; float: right; margin-right: 36%;">
+			</a>
 
 
 		</div>
 
-	</a>
-	<a  href="gestionar_tour.php">
-		<input type="button" value="Gestionar" style="color: white; background-color: green; border: solid; border-color: green; text-align: center; margin-top: -8.8%; margin-left: 70%; padding-left: 4%; padding-right: 5%;padding-top: 2%; padding-bottom: 2%; float: right; margin-right: 18%;">
-	</a>
-</div>
 
 
 
 
 
 
-<meta charset="UTF-8">
-<title>Gestionar</title>
-<link rel="stylesheet" href="gestionar_tour.css">
-</head>
-<body>
-	<img src="camino_campo.jpg" alt="" class="imagen_fondo">
-	<div class="barra_navegacion">
-		<a href="principal_registrado.php">
-			<img src="logo.png" alt="" class="logo">
-		</a>
+
 	</div>
-	<a href="perfil_usuario.php">
-		<img src="perfil.jpg" alt="" class="perfil_imagen">
-	</a>
-	<div class="sesion_usuario">
-		<center>
-			Hola
-			<?php
-			echo $_SESSION['nombre_cliente'];
-			?>
-			
-		</center>
-	</div>
-	<form action="cerrar_sesion.php" method="post">
-		<div class="boton">
-			<input type="button" value="Cerrar Sesion" id="botonsito" onclick="redireccion_cerrar_sesion()"> 
-		</div>
-	</form>
+
 	
-	<footer class="footer">
-		<div class="siguenos">
-			<b>Siguenos</b>
-			<center>
-				<a href="https://www.facebook.com/Bop-tour-391667081681891/?modal=admin_todo_tour" target="_blank">
-					<img src="fb.png" alt="" class="facebook">
-				</a>
-				<a href="https://www.instagram.com/bop_tour/" target="_blank">
-					<img src="insta.png" alt="" class="instagram">
-				</a>
-				<a href="https://twitter.com/BopTour" target="_blank">
-					<img src="twitter.png" alt="" class="twitter">
-				</a>
-			</center>
-		</div>
-	</footer>
+
+
+
+
+	
 </body>
 </html>
