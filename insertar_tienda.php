6d052fdc0bbe6b5f20ddd135760ed8bdc6d0ef1f
Oscar Matias Perez Epuante <operez@ing.ucsc.cl>
2019-07-02 22:47:00 -0400

    nose quw wea

--- insertar_tienda.php
+++ insertar_tienda.php
@@ -2,22 +2,22 @@
 
     session_start();
 	
-	$conexion2 = mysqli_connect("localhost", "root", "", "bop");
+	$conexion4 = mysqli_connect("localhost", "root", "", "bop");
 
 
-	$nombre_tienda = $_POST['nombre_tienda'];
-	$ubicacion = $_POST['ubicacion'];
+	$nombre_tienda = $_POST["nombre_tienda"];
+	$ubicacion = $_POST["ubicacion"];
     $ID_productor = $_POST["ID_productor"];
-	$rubro = $_POST['rubro'];
+	$rubro = $_POST["rubro"];
 
 
-	$consulta21 = "INSERT INTO tienda ('nombre_tienda','ubicacion','ID_productor','rubro','imagen') VALUES 
-    ($nombre_tienda','$ubicacion','$ID_productor','$rubro','NULL')";
+	$consulta21 = "INSERT INTO tienda VALUES 
+    ('', $nombre_tienda','$ubicacion','$ID_productor','$rubro','')";
 
-	$resultado21 = mysqli_query($conexion2, $consulta21);
+	$resultado21 = mysqli_query($conexion4, $consulta21);
 
     
     echo '<script>alert("Tienda publicada con exito")</script>';
-//    echo "<script>location.href='perfil_productor.php'</script>";
+    echo "<script>location.href='perfil_productor.php'</script>";
 
 ?>
