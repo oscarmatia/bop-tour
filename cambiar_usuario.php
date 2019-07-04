1daf495f8b28aea45d1f8c809212a58db4d52ef6
Oscar Matias Perez Epuante <operez@ing.ucsc.cl>
2019-07-04 00:21:32 -0400

    boton de cambiarse a productor

---
+++ cambiar_usuario.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Cambiar de usuario a productor</title>
+    <link rel="stylesheet" href="cambiar.css">
+</head>
+<body>
+    
+</body>
+</html>
