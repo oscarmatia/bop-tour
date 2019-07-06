5ac7a1a974d622c8e6aa3b967bb3ffafc63b3649
braggio2 <50121186+braggio2@users.noreply.github.com>
2019-07-06 17:05:37 -0400

    asdasd

--- mis_productos.php
+++ mis_productos.php
@@ -1,9 +1,12 @@
-<?php
-$conexion = new mysqli("localhost", "root", "", "bop-tour");
-$consulta = "SELECT * FROM producto_artesanal";
-$resultado = mysqli_query($conexion,$consulta);
-?>
+<?php 
+include 'connect.php';
+$con = conectar();
+session_start();
 
+$consulta2='SELECT id, marca, descripcion, sku, precio from producto_artesanal';
+$resultado=mysqli_query($con,$consulta2);
+
+?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -14,38 +17,6 @@ $resultado = mysqli_query($conexion,$consulta);
 <link rel="stylesheet" type="text/css" href="publicar_producto_artesanal.css">
 <link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Montez|Pathway+Gothic+One" rel="stylesheet">
 <body>
-
-	<script type="text/javascript">
-
-		function ingresarAlumno(){
-			var titulo = document.getElementById("titulo_anuncio").value;
-			var categoria = document.getElementById("categoria_producto").value;
-			var descripcion = document.getElementById("descripcion").value;
-			var precio = document.getElementById("precio").value;
-			var region = document.getElementById("region").value;
-			var comuna = document.getElementById("comuna").value;
-			var cantidad = document.getElementById("cantidad").value;
-
-
-
-			var data = '&titulo='+titulo+'categoria='+categoria+'&descripcion='+descripcion+'&precio='+precio+'&region='+region+'&comuna='+comuna+'&cantidad='+cantidad;
-
-			$.ajax({
-				type: 'POST',
-				url: 'guardar_producto.php',
-				data: data,
-				beforeSend: function() {
-					console.log(data);
-				},
-				success: function(data) {
-					console.log(data);
-					location.reload();
-				}
-			})
-			return false;
-		};
-	</script>
-
 
 
 
@@ -58,11 +29,17 @@ $resultado = mysqli_query($conexion,$consulta);
 
 		<div class="iniciar-sesion">
 
-			<div class="logo">
-				<img src="logo.png">
+			<a href="perfil_usuario.php">
+				<img src="perfil.jpg" alt="" class="perfil_imagen">
+			</a>
+			<div class="sesion_usuario">
+				<center>
+					Hola
+					<?php
+					echo $_SESSION['nombre_productor'];
+					?>
+				</center>
 			</div>
-
-			<a class="ir-a-inicio" href="principal.php">Ir a inicio</a>
 
 		</div>
 
@@ -100,39 +77,7 @@ $resultado = mysqli_query($conexion,$consulta);
 
 			<center>
 				
-<!-- /*
-				<table style="width:20%;text-align:center">
-					<tr>
-						<th>Titulo</th>
-						<th>Categoria</th>
-						<th>Descripcion</th>
-						<th>Precio</th>
-						<th>Region</th>
-						<th>Comuna</th>
-						<th>Cantidad</th>
 
-					</tr>
-
-
-
-
-					<?php
-					while($row=mysqli_fetch_assoc($resultado)){
-						echo "<tr>";
-						echo "<td>".$row["titulo"]."</td>";
-						echo "<td>".$row["categoria"]."</td>";
-						echo "<td>".$row["descripcion"]."</td>";
-						echo "<td>".$row["precio"]."</td>";
-						echo "<td>".$row["region"]."</td>";
-						echo "<td>".$row["comuna"]."</td>";
-						echo "<td>".$row["cantidad"]."</td>";
-
-						echo "</tr>";
-					}
-					?>
-				</table>
-
-				/* -->
 
 			</center>
 
@@ -148,14 +93,5 @@ $resultado = mysqli_query($conexion,$consulta);
 	</div>
 
 
-
-
-
-
-
-
-
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </body>
 </html>
--- ventana_resultado_producto.php
+++ ventana_resultado_producto.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,9 +7,76 @@
 	<meta charset="UTF-8">
 	<title>Publicado</title>
 </head>
+<link rel="stylesheet" type="text/css" href="publicar_producto_artesanal.css">
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Montez|Pathway+Gothic+One" rel="stylesheet">
 <body>
 
-	<H1>PRODUCTO PUBLICADO CORRECTAMENTE</H1>
+	<img class="imagen-campo" src="camino_campo.jpg">
+
+	<div class="barra-1">
+		<div class="iniciar-sesion">
+
+			<div class="logo">
+				<img src="logo.png">
+			</div>
+			<a href="perfil_usuario.php">
+				<img src="perfil.jpg" alt="" class="perfil_imagen">
+			</a>
+			<div class="sesion_usuario">
+				<center>
+					Hola
+					<?php
+					echo $_SESSION['nombre_cliente'];
+					?>
+				</center>
+			</div>
+			<form action="cerrar_sesion.php" method="post">
+				<div class="boton">
+					<input type="button" value="Cerrar Sesion" id="botonsito" onclick="redireccion_cerrar_sesion()"> 
+				</div>
+			</form>
+
+			
+
+		</div>
+
+	</div>
+
+	<div class="barra-2">
+
+		<div>
+			<ul class="list-1">
+
+				<li class="li1" id="efect"><a href="publicar_producto_artesanal.php">Publicar producto artesanal</a></li>
+				<li class="li1" id="efect"><a href="mis_productos.php">Mis productos</a></li>
+
+			</ul>
+		</div>	
+
+
+
+	</div>
+
+	<div class="promociona-text">
+
+		<h1 style="color: white;">Producto publicado correctamente</h1>
+		<hr width="80%;">	
+
+	</div>
+
 	
+	<H1>PRODUCTO PUBLICADO CORRECTAMENTE</H1>
+	<form action="perfil_productor.php">
+
+
+		<input style="background-color: black;margin-left: 4%" class="btn blue" type="submit" value="Volver">
+
+
+	</form>
+
+
 </body>
+
+
+
 </html>
